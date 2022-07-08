e0cf892efbe1d7cd6e72061188e4b73c8441072a
1nsenter <108155945+1nsenter@users.noreply.github.com>
2022-07-08 19:35:11 +0300

    Add files via upload

--- Rock Paper Scissors/Project1/Program.cs
+++ Rock Paper Scissors/Project1/Program.cs
@@ -6,13 +6,13 @@ namespace Progect1
     {
         static void Main()
         {
-            int i = 1, p1, p2;
+            int i = 1, p1, p2, Player1Score = 0, Player2Score = 0;
             string? PlayerName1, PlayerName2;
 
             Random random = new Random();
 
             Console.WriteLine("Добро пожаловать в игру \"Камень, Ножницы, Бумага\"");
-            Console.Write("Введите имя Первого Игрока: ");
+            Console.Write("\nВведите имя Первого Игрока: ");
 
             PlayerName1 = Console.ReadLine();
 
@@ -22,6 +22,7 @@ namespace Progect1
 
             while (i >= 1)
             {
+                Console.WriteLine($"Счет: {PlayerName1} - {Player1Score} | {Player2Score} - {PlayerName2}");
                 Console.WriteLine("\nДля начала игры введите слово \"start\" или для выхода \"quit\"");
                 string? result = Console.ReadLine();
                 if (result == "start")
@@ -33,48 +34,54 @@ namespace Progect1
                     // 2 = бумага
                     if (p1 == 0 && p2 == 1)
                     {
-                        Console.WriteLine($"{PlayerName1} = ножницы | камень = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = ножницы | камень = {PlayerName2}");
                         Console.WriteLine($"Результат: Победил {PlayerName2}");
+                        Player2Score += 1;
                     }
                     else if (p1 == 0 && p2 == 2)
                     {
-                        Console.WriteLine($"{PlayerName1} = ножницы | бумага = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = ножницы | бумага = {PlayerName2}");
                         Console.WriteLine($"Результат: Победил {PlayerName1}");
+                        Player1Score += 1;
                     }
                     else if (p1 == 0 && p2 == 0)
                     {
-                        Console.WriteLine($"{PlayerName1} = ножницы | ножницы = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = ножницы | ножницы = {PlayerName2}");
                         Console.WriteLine($"Результат: Ничья");
                     }
                     else if (p1 == 1 && p2 == 1)
                     {
-                        Console.WriteLine($"{PlayerName1} = камень | камень = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = камень | камень = {PlayerName2}");
                         Console.WriteLine($"Результат: Ничья");
                     }
                     else if (p1 == 1 && p2 == 0)
                     {
-                        Console.WriteLine($"{PlayerName1} = камень | ножницы = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = камень | ножницы = {PlayerName2}");
                         Console.WriteLine($"Результат: Победил {PlayerName1}");
+                        Player1Score += 1;
                     }
                     else if (p1 == 1 && p2 == 2)
                     {
-                        Console.WriteLine($"{PlayerName1} = камень | бумага = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = камень | бумага = {PlayerName2}");
                         Console.WriteLine($"Результат: Победил {PlayerName2}");
+                        Player2Score += 1;
                     }
                     else if (p1 == 2 && p2 == 2)
                     {
-                        Console.WriteLine($"{PlayerName1} = бумага | бумага = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = бумага | бумага = {PlayerName2}");
                         Console.WriteLine($"Результат: Ничья");
                     }
                     else if (p1 == 2 && p2 == 0)
                     {
-                        Console.WriteLine($"{PlayerName1} = бумага | ножницы = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = бумага | ножницы = {PlayerName2}");
                         Console.WriteLine($"Результат: Победил {PlayerName2}");
+                        Player2Score += 1;
                     }
                     else if (p1 == 2 && p2 == 1)
                     {
-                        Console.WriteLine($"{PlayerName1} = бумага | камень = {PlayerName2}");
+                        Console.WriteLine($"\n{PlayerName1} = бумага | камень = {PlayerName2}");
                         Console.WriteLine($"Результат: Победил {PlayerName1}");
+                        Player1Score += 1;
                     }
                 }
                 else if (result == "quit")
